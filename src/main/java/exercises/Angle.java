759ffeab4378c8a393b407c0ff4d61c450106e28
Andrew <andrewc.london2019a@gmail.com>
2020-01-07 15:10:31 +0000

    Adding negtive angle test successful

--- src/main/java/exercises/Angle.java
+++ src/main/java/exercises/Angle.java
@@ -4,8 +4,13 @@ public class Angle {
     private int degrees;
 
     public Angle(int degrees) {
+        if (degrees < 0) {
+           while (degrees < 0) {
+               degrees += 360;
+           }
+        }
         setDegrees(degrees);
-    }
+        }
 
     public int getDegrees() {
         return degrees;
--- src/test/java/exercises/AngleTest.java
+++ src/test/java/exercises/AngleTest.java
@@ -11,9 +11,21 @@ class AngleTest {
     public void setUp() throws Exception {}
 
     @Test
-    public void test() {
+    public void angle_same_as_degrees() {
         Angle a = new Angle(10);
         assertEquals(10,a.getDegrees(),"They are not equal");
+    }
+
+    @Test
+    public void is_angle_added_if_negative() {
+        Angle a = new Angle(-10);
+        assertEquals(350,a.getDegrees(),"They are not equal");
+    }
+
+    @Test
+    public void is_large_angle_added_if_negative() {
+        Angle a = new Angle(-370);
+        assertEquals(350,a.getDegrees(),"They are not equal");
     }
 
 }
